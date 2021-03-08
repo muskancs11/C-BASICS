7f101c844e9b9c7eb6028e3a7eae5dabce586280
MK2346 <70057473+MK2346@users.noreply.github.com>
2021-03-08 16:34:52 +0530

    Add files via upload

---
+++ Subtraction.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("ENTER FIRST NUMBER:\n");
+	scanf("%d",&a);
+	printf("ENTER SECOND NUMBER:\n");
+	scanf("%d",&b);
+	c=a-b;
+	printf("SUBTRACTION OF ABOVE NUMBERS IS:""%d",c);
+}
