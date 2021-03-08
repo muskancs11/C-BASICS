b7d9eda74e1509380f48cb2609c09050601b2847
MK2346 <70057473+MK2346@users.noreply.github.com>
2021-03-08 17:39:08 +0530

    Create Addition Printing Whole Result Using Variables.c

---
+++ Addition Printing Whole Result Using Variables.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("ENTER FIRST NUMBER:");
+	scanf("%d",&a);
+	printf("ENTER SECOND NUMBER:");
+	scanf("%d",&b);
+	c=a+b;
+	printf("Sum of Above NUMBERS IS: ""%d + %d = %d", a, b, c);
+		return 0;
+}
